f9bc390a77d2e068b5436038d532876fa158bdee
Akshay Deshmukh <Akshay.Deshmukh@datacom.co.nz>
2020-07-15 12:29:12 +1200

    Add new events functionality

---
+++ src/features/event/EventForm/EventForm.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from "react"
+import { Form, Segment, Button } from "semantic-ui-react"
+
+export default class EventForm extends Component {
+  state = {
+    title: "",
+    date: "",
+    city: "",
+    venue: "",
+    hostedBy: "",
+  }
+
+  handleInputValueChange = ({target: {name, value}}) => {
+    this.setState({
+      [name]:value,
+    })
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.createNewEvent(this.state);
+  }
+
+  render() {
+    const { cancelFormOpen } = this.props
+    const { title, date, city, venue, hostedBy } = this.state
+
+    return (
+      <Segment>
+        <Form onSubmit={this.handleFormSubmit}>
+          <Form.Field>
+            <label>Event Title</label>
+            <input
+              name='title'
+              value={title}
+              onChange={this.handleInputValueChange}
+              placeholder='Title'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Event Date</label>
+            <input
+              name='date'
+              value={date}
+              onChange={this.handleInputValueChange}
+              type='date'
+              placeholder='Event Date'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>City</label>
+            <input
+              name='city'
+              value={city}
+              onChange={this.handleInputValueChange}
+              placeholder='City event is taking place'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Venue</label>
+            <input
+              name='venue'
+              value={venue}
+              onChange={this.handleInputValueChange}
+              placeholder='Enter the Venue of the event'
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Hosted By</label>
+            <input
+              name='hostedBy'
+              value={hostedBy}
+              onChange={this.handleInputValueChange}
+              placeholder='Enter the name of person hosting'
+            />
+          </Form.Field>
+          <Button positive type='submit'>
+            Submit
+          </Button>
+          <Button type='button' onClick={cancelFormOpen}>
+            Cancel
+          </Button>
+        </Form>
+      </Segment>
+    )
+  }
+}
---
+++ src/features/event/EventList/EventListItem.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react"
+import { Segment, Item, List, Button, Icon } from "semantic-ui-react"
+import EventListAttendes from "./EventListAttendes"
+
+export default class EventListItem extends Component {
+  render() {
+    const { event } = this.props
+    return (
+      <Segment.Group>
+        <Segment>
+          <Item.Group>
+            <Item>
+              <Item.Image size='tiny' circular src={event.hostPhotoURL} />
+              <Item.Content>
+                <Item.Header as='a'>{event.title} </Item.Header>
+                <Item.Description>
+                  Hosted by{" "}
+                  <a href='https://react.semantic-ui.com/addons/responsive/#types-content'>
+                  {event.hostedBy} 
+                  </a>
+                </Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+        <Segment>
+          <span>
+            <Icon name='clock' /> {event.date}  |
+            <Icon name='marker' /> {event.time} 
+          </span>
+        </Segment>
+        <Segment secondary>
+          <List horizontal>
+              {event.attendees && event.attendees.map(attendees => (
+                   <EventListAttendes key={attendees.id} attendees={attendees}/>
+              ))}             
+          </List>
+        </Segment>
+        <Segment clearing>
+          <span>{event.description} </span>
+          <Button as='a' color='teal' floated='right' content='View' />
+        </Segment>
+      </Segment.Group>
+    )
+  }
+}
